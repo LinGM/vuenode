60f3259ca5141772589aea2c555231c8946be8a1
LinGM <544374882@qq.com>
2018-03-27 19:15:27 +0800

    modify db.js

--- server/db.js
+++ server/db.js
@@ -1,9 +1,9 @@
 var mysql      = require('mysql');
 // var connection = mysql.createConnection({
-//   host     : 'qdm204983338.my3w.com',
-//   user     : 'qdm204983338',
-//   password : 'hs3410040434lgm',
-//   database : 'qdm204983338_db'
+//   host     : '****',
+//   user     : '****',
+//   password : '****',
+//   database : '****'
 // });
 
 var config = {
